191de54cba4112fcb9c4b49fae4720a8117d7486
tsuihu <1335973535@qq.com>
2022-11-07 10:02:01 +0800

    注册优化

--- src/views/Registe.vue
+++ src/views/Registe.vue
@@ -1,38 +1,42 @@
 <template>
   <div class="registe">
-    <h1>账号注册</h1>
-    <van-form class="form_registe" @submit="onSubmit">
-      <van-cell-group inset>
-        <van-field 
-          v-model="RegisteForm.tel" 
-          name="手机号" 
-          label="手机号" 
-          placeholder="手机号"
-          :rules="[{ required: true, message: '请填写手机号' }]" />
-        <van-field 
-          v-model="RegisteForm.password" 
-          type="password" 
-          name="密码" 
-          label="密码" 
-          placeholder="密码"
-          :rules="[{ required: true, message: '请填写密码' }]" />
-        <van-field 
-          v-model="RegisteForm.name" 
-          name="用户名" 
-          label="用户名" 
-          placeholder="用户名"
-          :rules="[{ required: true, message: '请填写用户名' }]" />
-        <van-field 
-          v-model="RegisteForm.idcard" 
-          name="身份证" 
-          label="身份证" 
-          placeholder="身份证"
-          :rules="[{ required: true, message: '请填写身份证号码' }]" />
-      </van-cell-group>
-      <van-button class="btn_registe" round block type="primary" native-type="submit">
-        注册
-      </van-button>
-    </van-form>
+    <div class="container">
+      <h1>账号注册</h1>
+      <van-form class="form_registe" @submit="onSubmit">
+        <van-cell-group inset>
+          <van-field 
+            v-model="RegisteForm.tel" 
+            name="手机号" 
+            label="手机号" 
+            placeholder="手机号"
+            :rules="[{ required: true, message: '请填写手机号' }]" />
+          <van-field 
+            v-model="RegisteForm.password" 
+            type="password" 
+            name="密码" 
+            label="密码" 
+            placeholder="密码"
+            :rules="[{ required: true, message: '请填写密码' }]" />
+          <van-field 
+            v-model="RegisteForm.name" 
+            name="用户名" 
+            label="用户名" 
+            placeholder="用户名"
+            :rules="[{ required: true, message: '请填写用户名' }]" />
+          <van-field 
+            v-model="RegisteForm.idcard" 
+            name="身份证" 
+            label="身份证" 
+            placeholder="身份证"
+            :rules="[{ required: true, message: '请填写身份证号码' }]" />
+        </van-cell-group>
+        <van-button class="btn_registe" round block type="primary" native-type="submit">
+          注册
+        </van-button>
+      </van-form>
+      <p>已有账号，去<router-link class="btn_login" to="/login">登录</router-link></p>
+    </div>
+    
   </div>
 </template>
 
@@ -85,6 +89,9 @@ export default {
     float: right;
     margin-top: 20/40rem;
     font-size: 30/40rem;
+  }
+  .btn_login {
+    color: skyblue;
   }
   .btn_registe {
     margin-top: 100/40rem;
